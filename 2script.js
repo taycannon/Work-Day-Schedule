b4e28d8a9f430efb3402532ae1d57dbfff93e220
taycannon <taycannon2003@gmail.com>
2023-09-28 17:23:38 -0500

    Added input for times and save button, also deleted 1st index & js

--- 2script.js
+++ 2script.js
@@ -1,13 +1,56 @@
-var tableRef = $("#projects");
-var projectForm = $("#project-form");
-var projectsList = localStorage.getItem("projects") || [];
-
-
+// Function to render the current time
 function renderTime() {
     $("#current-time").text(dayjs().format("MMM DD, YYYY [at] hh:mm:ss a"));
-}
-renderTime();
-setInterval(renderTime, 1000);
+  }
+  
+  // Function to save events to localStorage
+  function saveEvent(hour) {
+    const eventInput = $(`#event-${hour}`);
+    const eventText = eventInput.val();
+    localStorage.setItem(`event-${hour}`, eventText);
+  }
+  
+  // Function to load events from localStorage
+  function loadEvents() {
+    for (let hour = 9; hour <= 17; hour++) {
+      const eventText = localStorage.getItem(`event-${hour}`);
+      if (eventText) {
+        $(`#event-${hour}`).val(eventText);
+      }
+    }
+  }
+  
+  // Function to handle save button clicks
+  $(".save-button").on("click", function () {
+    const hour = $(this).data("hour");
+    saveEvent(hour);
+  });
+  
+  // Initial rendering of time and loading of events
+  renderTime();
+  loadEvents();
+  
+  // Update time every second
+  setInterval(renderTime, 1000);
+  
+
+
+
+
+
+
+
+
+// var tableRef = $("#projects");
+// var projectForm = $("#project-form");
+// var projectsList = localStorage.getItem("projects") || [];
+
+
+// function renderTime() {
+//     $("#current-time").text(dayjs().format("MMM DD, YYYY [at] hh:mm:ss a"));
+// }
+// renderTime();
+// setInterval(renderTime, 1000);
 
 // projectForm.on("submit", function (event) {
 //     event.preventDefault();
